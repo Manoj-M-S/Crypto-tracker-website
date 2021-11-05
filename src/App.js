26e74e1d713daa5ac49223b6233f0c3295edf6c8
Manoj M S <manoj.makarasu@gmail.com>
2021-11-05 08:00:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Recentlyadded from "./Components/recentlyadded";
+import MostVisited from "./Components/mostvisited";
+import Top10 from "./Components/top10";
+import Topgainers from "./Components/topgainers";
+import Toplosers from "./Components/toplosers";
+import Toptrending from "./Components/trending";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/recentlyadded" exact component={Recentlyadded} />
+        <Route path="/mostvisited" exact component={MostVisited} />
+        <Route path="/top10" exact component={Top10} />
+        <Route path="/topgainers" exact component={Topgainers} />
+        <Route path="/toplosers" exact component={Toplosers} />
+        <Route path="/trending" exact component={Toptrending} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
